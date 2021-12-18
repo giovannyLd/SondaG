3f045f34da4e244eea46bdbc7446c3a159ae8701
giova <giova@DESKTOP-F6UQNN1>
2021-12-18 11:08:18 -0500

    conexion base datos firestore

--- app/src/main/java/com/example/sondag/MainActivity.kt
+++ app/src/main/java/com/example/sondag/MainActivity.kt
@@ -4,14 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    var db = FirebaseFirestore.getInstance()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun goMainActivity(view: android.view.View) {
+
+        db.collection("Formulario").document("giovanny").set(
+
+            hashMapOf(
+                "nombre" to "giovanny fandiño motnes "
+
+        ))
 
         val start = Intent(this,start_activity::class.java)
         startActivity(start)
